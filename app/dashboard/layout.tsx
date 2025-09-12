64c2af02b8048b7dab639f15d8e4040febbfc71e
mcmaina <irutujmaina@gmail.com>
2025-09-12 10:38:06 +0300

    added sidebar layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import { AdminSidebar } from "@/components/sidebar/admin-sidebar";
+import { DashboardHeader } from "@/components/sidebar/dashboard-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AdminSidebar />
+
+      <SidebarInset>
+        <DashboardHeader />
+
+        <div className="p-2 pt-0 sm:gap-4 sm:p-4">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
